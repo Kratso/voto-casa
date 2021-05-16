a117fdb539718d1711fd83e65b398fd1700bc398
@garcianevesmateo <garcianevesmateo@gmail.com>
2021-05-16 16:28:44 +0200

    feat: added dnd and dialogs to voting list #0

--- src/components/casas/CasaDialog.tsx
+++ src/components/casas/CasaDialog.tsx
@@ -1,11 +1,53 @@
+import { Dialog, InputGroup, Label } from '@blueprintjs/core';
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
 import { Casa } from '../../services/casas/index';
+import { formatNumber } from '../votos/Votacion';
 
 
 interface CasaDialogProps {
-	casa: Casa
+	casa: Casa,
+	toggleOpen: ()=>void,
+	isOpen: boolean,
 }
 
 export class CasaDialog extends React.PureComponent<CasaDialogProps>{
-	
+	render(){
+		const {nombre, localizacion, imagenes, precio, numeroDeHuespedes} = this.props.casa
+
+		return (
+			<Dialog
+				className="bp3-dark casa-dialog-container"
+				isOpen={this.props.isOpen}
+				isCloseButtonShown={false}
+				onClose={this.props.toggleOpen}
+				title={nombre}
+			>
+				<div className="casa-dialog">
+					<h2>{localizacion}</h2>
+					<Label className="bp3-inline">
+						Precio por Noche: <InputGroup disabled leftIcon="euro" value={formatNumber(precio)} />
+					</Label>
+					<Label className="bp3-inline">
+						Número de Huéspedes: <InputGroup disabled leftIcon="person" value={numeroDeHuespedes.toString()} />
+					</Label>
+					<Carousel 
+						className="carrousel"
+						autoPlay={true}
+						centerMode={true}
+						centerSlidePercentage={70}
+						dynamicHeight={false}
+						infiniteLoop={true}
+
+					>
+						{imagenes.map(img=>(
+							<div>
+								<img src={img} alt={nombre + " imagen"} />
+							</div>
+						))}
+					</Carousel>
+				</div>
+			</Dialog>
+		)
+	}
 }
--- src/components/votos/Votacion.tsx
+++ src/components/votos/Votacion.tsx
@@ -1,17 +1,37 @@
-import { Card } from '@blueprintjs/core';
+import { Card, InputGroup, Label } from '@blueprintjs/core';
 import React, { useState }  from 'react';
  import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { Casa } from '../../services/casas/index';
+import { CasaDialog } from '../casas/CasaDialog';
+import { Classes } from "@blueprintjs/core";
 
 
 interface IVotacionesProps {
 	casas: Casa[],
 	updateCasas: (casas: Casa[])=>void
 }
+
+export const formatNumber = (num: number)=>{
+	const intl = new Intl.NumberFormat('es-ES', {style: 'currency', currency: "EUR"});
+	const fullFormat = intl.format(num);
+
+	return fullFormat.substring(0, fullFormat.length-2);
+}
+
+
+
+
 export const Votaciones : React.FunctionComponent<IVotacionesProps> = (props:IVotacionesProps) => {
 
 	const [casas, updateCasas] = useState(props.casas);
+	const [openCasaDialog, toggleCasaDialog] = useState(false);
+	const [currentCasa, updateCasa] = useState(props.casas[0]);
 
+	const handleOpenDialogCasa = (casa : Casa)=>{
+		toggleCasaDialog(!openCasaDialog)
+		updateCasa(casa)
+	}
+	
 	const handleOnDragEnd = (result: any)=>{
 		if (!result.destination) return;
 		const items = Array.from(casas);
@@ -22,33 +42,58 @@ export const Votaciones : React.FunctionComponent<IVotacionesProps> = (props:IVo
 		props.updateCasas(items)
 	}
 
+	const containerClases = "votacion-container " + Classes.CARD
+
 	return (
-		<div className="lista-votacion">
-			<DragDropContext  onDragEnd={handleOnDragEnd}>
-				<Droppable droppableId="casas">
-					{(provided)=> (
-						<ul className="lista-casas" {...provided.droppableProps} ref={provided.innerRef}>
-							{
-								casas.map((({nombre, localizacion, imagenes, precio, numeroDeHuespedes}, index) => (
-									<Draggable key={nombre} draggableId={nombre} index={index}>
-										{
-											(provided)=>(
-												<li  ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-													<Card>
-														{localizacion + "\n" + imagenes[0]+"\n"+precio+"\n"+numeroDeHuespedes}
-													</Card>
-												</li>
-											)
-										}
-										
-									</Draggable>
-								)))
-							}
-							{provided.placeholder}
-						</ul>
-					)}
-				</Droppable>
-			</DragDropContext>
+		<div className={containerClases}>
+			<div className="votacion-exp">
+				<h1>Votación por Ranking</h1>
+				<p>
+					Ordena la lista de casas por órden de favorito, la primera siendo la que más te guste, y la última la que menos.
+				</p>
+				<p>
+					Se hará un cómputo de todas las puntuaciones para cada casa para decidir la casa ganadora.
+				</p>
+				<p>
+					Para más información sobre las casas, haz click sobre cualquier de ellas.
+				</p>
+			</div>
+			<div className="lista-votacion">
+				<DragDropContext  onDragEnd={handleOnDragEnd}>
+					<Droppable droppableId="casas">
+						{(provided)=> (
+							<ul className="lista-casas" {...provided.droppableProps} ref={provided.innerRef}>
+								{
+									casas.map((({nombre, localizacion, imagenes, precio, numeroDeHuespedes}, index) => (
+										<Draggable key={nombre} draggableId={nombre} index={index}>
+											{
+												(provided)=>(
+													<li  ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+														<Card interactive={true} onClick={()=>handleOpenDialogCasa({nombre, localizacion, imagenes, precio, numeroDeHuespedes})} className="casa-card">
+															<h2>{nombre}</h2>
+															<h3>{localizacion}</h3>
+															<img src={imagenes[0]} alt="Portada" />
+															<Label className="bp3-inline">
+																Precio: <InputGroup disabled leftIcon="euro" value={formatNumber(precio)} />
+															</Label>
+															<Label className="bp3-inline">
+																Número de Huéspedes: <InputGroup disabled leftIcon="person" value={numeroDeHuespedes.toString()} />
+															</Label>														
+														</Card>
+													</li>
+												)
+											}
+											
+										</Draggable>
+									)))
+								}
+								{provided.placeholder}
+							</ul>
+						)}
+					</Droppable>
+				</DragDropContext>
+			</div>
+			<CasaDialog casa={currentCasa} isOpen={openCasaDialog} toggleOpen={()=>{handleOpenDialogCasa(currentCasa)}} />
 		</div>
 	)
 }
